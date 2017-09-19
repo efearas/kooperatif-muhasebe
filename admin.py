628a17548562a8d639bbee40b79ed1a13b0a341c
Efe Aras <tugceesenkar@em-tugce-e.boun>
2017-09-19 09:30:10 +0300

    admin'e butun modeller eklendi

--- admin.py
+++ admin.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
-from .models import urun,uretici,Satis,SatisStokHareketleri,Birim,GiderTipi,VirmanVeDuzeltmeHesaplari,StokHareketiTipi,UrunKategorisi
+from .models import urun,uretici,Satis,SatisStokHareketleri,Birim,GiderTipi,VirmanVeDuzeltmeHesaplari,StokHareketiTipi,UrunKategorisi, Gider, StokGirisi, VirmanVeDuzeltme
 # Register your models here.
 admin.site.register(Birim)
 admin.site.register(GiderTipi)
 admin.site.register(Satis)
+admin.site.register(SatisStokHareketleri)
 admin.site.register(VirmanVeDuzeltmeHesaplari)
 admin.site.register(StokHareketiTipi)
 admin.site.register(UrunKategorisi)
+admin.site.register(Gider)
+admin.site.register(StokGirisi)
+admin.site.register(VirmanVeDuzeltme)
